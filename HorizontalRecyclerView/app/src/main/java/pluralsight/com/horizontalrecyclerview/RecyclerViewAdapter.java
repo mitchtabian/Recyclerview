6b001a50f15a80de9242c9152ed056366954dfed
Mitch <mitch@tabian.ca>
2018-02-12 09:26:44 -0800

    Horizontal RecyclerView

---
+++ HorizontalRecyclerView/app/src/main/java/pluralsight/com/horizontalrecyclerview/RecyclerViewAdapter.java
@@ -0,0 +1,79 @@
+package pluralsight.com.horizontalrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by User on 2/12/2018.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> names, ArrayList<String> imageUrls) {
+        mNames = names;
+        mImageUrls = imageUrls;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImageUrls.get(position))
+                .into(holder.image);
+
+        holder.name.setText(mNames.get(position));
+
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked on an image: " + mNames.get(position));
+                Toast.makeText(mContext, mNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageUrls.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView image;
+        TextView name;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image_view);
+            name = itemView.findViewById(R.id.name);
+        }
+    }
+}
